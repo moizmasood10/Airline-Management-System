ca1a68da0f39d6ed0ee0fe8fa1a88fb3d09898e4
Ashhad Zafar Khan <94891509+AshhadK@users.noreply.github.com>
2023-06-02 01:29:33 +0500

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "ls-ce1eb672c2222fb22bf65c82d0b2d086c7de3f40.cdb7mxbdnxoe.ap-southeast-1.rds.amazonaws.com";
 $username = "dbmasteruser";
 $password = "ecom$1234";
-$database = "Database-2";
+$database = "dbmaster";
 
 // Create a connection
 $conn = mysqli_connect($servername, $username, $password, $database);
